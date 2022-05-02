6868adec97dd94209850e2ccd07a5496eb3e8917
NekokeCore <Core2002@163.com>
2022-05-02 15:52:13 +0800

    添加了清除所有备份的功能

--- src/main/java/fun/fifu/tool/test/PlayerInvBackup.java
+++ src/main/java/fun/fifu/tool/test/PlayerInvBackup.java
@@ -1,13 +1,21 @@
 package fun.fifu.tool.test;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.io.FileUtil;
+import fun.fifu.tool.test.controller.MongoController;
+import org.bson.Document;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PlayerInvBackup {
     public static Scanner scanner = new Scanner(System.in);
+    public static MongoController mongoController = new MongoController();
 
     public static void main(String[] args) {
         try {
-            System.out.print("1.备份  2.恢复  3.查看备份  4.删除备份  请选择操作（序号）：");
+            System.out.print("1.备份  2.恢复  3.查看备份  4.清除所有备份  请选择操作（序号）：");
             switch (scanner.next()) {
                 case "1" -> backup();
                 case "2" -> rollback();
@@ -20,6 +28,9 @@ public class PlayerInvBackup {
     }
 
     private static void delBackups() {
+        TimeInterval timer = DateUtil.timer();
+        mongoController.getCollection().find(new Document()).forEach(document -> mongoController.getCollection().deleteOne(document));
+        System.out.println("清除完毕，耗时：" + timer.intervalMs() + "毫秒");
     }
 
     private static void viewBackups() {
@@ -29,5 +40,8 @@ public class PlayerInvBackup {
     }
 
     private static void backup() {
+        TimeInterval timer = DateUtil.timer();
+        Arrays.stream(FileUtil.file(DataManger.configPojo.getWorld_playerdata_path()).list()).filter(file -> file.endsWith(".dat")).forEach(file -> mongoController.backupWithUUID(file.substring(0, file.lastIndexOf("."))));
+        System.out.println("备份成功，耗时：" + timer.intervalMs() + "毫秒");
     }
 }
--- src/test/java/fun/fifu/tool/test/Test.java
+++ src/test/java/fun/fifu/tool/test/Test.java
@@ -1,21 +1,30 @@
 package fun.fifu.tool.test;
 
+import cn.hutool.core.io.FileTypeUtil;
+import cn.hutool.core.io.FileUtil;
 import fun.fifu.tool.test.controller.MongoController;
+
+import java.util.Arrays;
 
 public class Test {
     @org.junit.jupiter.api.Test
-    public void configReadTest(){
+    public void configReadTest() {
         System.out.println(DataManger.uuid2namePojo);
     }
 
     @org.junit.jupiter.api.Test
-    public void configSaveTest(){
+    public void configSaveTest() {
         DataManger.configPojo.setUuid2name_path("test_uuid2name.json");
         DataManger.saveData();
     }
 
     @org.junit.jupiter.api.Test
-    public void backupTest(){
+    public void backupTest() {
         new MongoController().backupWithUUID("00cc0e68-3261-45ac-9b21-ad796a0d98b7");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void foreachDatTest() {
+        Arrays.stream(FileUtil.file(DataManger.configPojo.getWorld_playerdata_path()).list()).filter(file -> file.endsWith(".dat")).forEach(file -> System.out.println(file.substring(0, file.lastIndexOf("."))));
     }
 }
