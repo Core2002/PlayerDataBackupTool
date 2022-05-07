c20cbe34f3efe013f4b6e4cd9b62bdde2421e99a
NekokeCore <Core2002@163.com>
2022-05-08 01:27:57 +0800

    添加了按时间批量回滚的功能

--- src/main/java/fun/fifu/tool/player_backup/PlayerInvBackup.java
+++ src/main/java/fun/fifu/tool/player_backup/PlayerInvBackup.java
@@ -2,11 +2,13 @@ package fun.fifu.tool.player_backup;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.text.StrFormatter;
 import fun.fifu.tool.player_backup.controller.MongoController;
 import fun.fifu.tool.player_backup.pojo.DataPojo;
 import org.bson.Document;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PlayerInvBackup {
     public static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
@@ -15,12 +17,13 @@ public class PlayerInvBackup {
     public static void main(String[] args) {
         try {
             while (true) {
-                System.out.print("1.备份  2.恢复  3.查看备份  4.清除所有备份  请选择操作（序号）：");
+                System.out.print("1.备份  2.恢复  3.查看备份  4.清除所有备份  5.按时间批量恢复  请选择操作（序号）：");
                 switch (scanner.next()) {
                     case "1" -> backup();
                     case "2" -> rollback();
                     case "3" -> viewBackups();
                     case "4" -> delBackups();
+                    case "5" -> rollbackAll();
                     default -> System.out.println("非法操作");
                 }
             }
@@ -65,7 +68,7 @@ public class PlayerInvBackup {
         String date = scanner.next();
         if (!dataPojo.containsKey(date)) {
             System.out.println("日期 " + date + " 不存在");
-            System.out.println("可能的日期："+dataPojo.keySet());
+            System.out.println("可能的日期：" + dataPojo.keySet());
             return;
         }
         TimeInterval timer = DateUtil.timer();
@@ -77,5 +80,32 @@ public class PlayerInvBackup {
         TimeInterval timer = DateUtil.timer();
         mongoController.backupAll();
         System.out.println("备份成功，耗时：" + timer.intervalMs() + "毫秒");
+    }
+
+    private static void rollbackAll() {
+        System.out.print("请输入要还原的日期：");
+        String date = scanner.next();
+
+        AtomicInteger total = new AtomicInteger();
+        AtomicInteger success = new AtomicInteger();
+        AtomicInteger fail = new AtomicInteger();
+
+        TimeInterval timer = DateUtil.timer();
+        mongoController.collection.find(new Document()).forEach(document -> {
+            DataPojo dataPojo = DataManger.gson.fromJson(document.toJson(), DataPojo.class);
+            Map<String, String> data = dataPojo.getData();
+            if (!data.containsKey(date)) {
+                fail.getAndIncrement();
+                System.out.println(StrFormatter.format("警告： 未发现玩家 <{}> (uuid:{}) 在 {} 的备份",
+                        DataManger.getName(dataPojo.getPlayer_uuid()), dataPojo.getPlayer_uuid(), date));
+            } else {
+                success.getAndIncrement();
+                DataManger.rollBackData(dataPojo.getPlayer_uuid(), data.get(date));
+            }
+            total.getAndIncrement();
+        });
+
+        System.out.println("报告：共 " + total.get() + " 个玩家，成功 " + success.get() + " 个，失败 " + fail.get() + " 个。");
+        System.out.println("恢复到 " + date + " 完毕，耗时：" + timer.intervalMs() + "毫秒，操作时间：" + DateUtil.now());
     }
 }
